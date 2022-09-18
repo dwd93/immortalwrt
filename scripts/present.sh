97b381ef10ec7adea647fc99532c192fb4548be2
lynxnexy <81577496+lynxnexy@users.noreply.github.com>
2022-09-18 23:13:59 +0700

    Update present.sh

--- scripts/present.sh
+++ scripts/present.sh
@@ -3,7 +3,7 @@
 # Copyright 2022 by lynxnexy <https://github.com/lynxnexy/immortalwrt>
 # 
 
-cat << EOF > package/luci-app-openclash/luasrc/view/openclash/editor.htm
+cat << EOF > feeds/luci/applications/luci-app-openclash/luasrc/view/openclash/editor.htm
 <%+header%>
 <div class="cbi-map">
 <iframe id="editor" style="width: 100%; min-height: 100vh; border: none; border-radius: 2px;"></iframe>
@@ -14,6 +14,6 @@ document.getElementById("editor").src = "http://" + window.location.hostname + "
 <%+footer%>
 EOF
 
-sed -i "s/yacd/Yet Another Clash Dashboard/g" package/luci-app-openclash/root/usr/share/openclash/ui/yacd/manifest.webmanifest
-sed -i '94s/80/90/g' package/luci-app-openclash/luasrc/controller/openclash.lua
-sed -i '94 i\	entry({"admin", "services", "openclash", "editor"}, template("openclash/editor"),_("Config Editor"), 80).leaf = true' package/luci-app-openclash/luasrc/controller/openclash.lua
+sed -i "s/yacd/Yet Another Clash Dashboard/g" feeds/luci/applications/luci-app-openclash/root/usr/share/openclash/ui/yacd/manifest.webmanifest
+sed -i '94s/80/90/g' feeds/luci/applications/luci-app-openclash/luasrc/controller/openclash.lua
+sed -i '94 i\	entry({"admin", "services", "openclash", "editor"}, template("openclash/editor"),_("Config Editor"), 80).leaf = true' feeds/luci/applications/luci-app-openclash/luasrc/controller/openclash.lua

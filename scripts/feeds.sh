09841e692e1497556f44a5b55f15599281973e05
dwd93 <115346138+dwd93@users.noreply.github.com>
2022-10-15 11:59:43 +0700

    Update feeds.sh

--- scripts/feeds.sh
+++ scripts/feeds.sh
@@ -8,9 +8,12 @@
 # Uncomment a feed source
 # sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+sed -i 's/\"#src/gz\"/\"src/gz\"/g' customfeeds.conf
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src/gz custom_generic https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/generic' customfeeds.conf
+sed -i '$a src/gz custom_arch https://raw.githubusercontent.com/lrdrdn/my-opkg-repo/main/$(cat /etc/os-release | grep OPENWRT_ARCH | awk -F '"' '{print $2}')' customfeeds.conf
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
